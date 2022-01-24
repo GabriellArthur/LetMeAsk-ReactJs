29f48bf3c9e7dff4d984a28fbceb9d83ee4396b5
GabriellArthur <gabriel.arthur2@gmail.com>
2022-01-24 18:20:15 -0300

    ok

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,68 +14,68 @@ import { useAuth } from '../hooks/useAuth';
 import '../styles/auth.scss';
 
 export function Home() {
-  const history = useHistory();
-  const { user, signInWithGoogle } = useAuth()
-  const [roomCode, setRoomCode] = useState('');
+   const history = useHistory();
+   const { user, signInWithGoogle } = useAuth()
+   const [roomCode, setRoomCode] = useState('');
 
-  async function handleCreateRoom() {
-    if (!user) {
-      await signInWithGoogle()
-    }
+   async function handleCreateRoom() {
+      if (!user) {
+         await signInWithGoogle()
+      }
 
-    history.push('/rooms/new');
-  }
+      history.push('/rooms/new');
+   }
 
-  async function handleJoinRoom(event: FormEvent) {
-    event.preventDefault();
+   async function handleJoinRoom(event: FormEvent) {
+      event.preventDefault();
 
-    if (roomCode.trim() === '') {
-      return;
-    }
+      if (roomCode.trim() === '') {
+         return;
+      }
 
-    const roomRef = await database.ref(`rooms/${roomCode}`).get();
+      const roomRef = await database.ref(`rooms/${roomCode}`).get();
 
-    if (!roomRef.exists()) {
-      alert('Room does not exists.');
-      return;
-    }
+      if (!roomRef.exists()) {
+         alert('Room does not exists.');
+         return;
+      }
 
-    if (roomRef.val().endedAt) {
-      alert('Room already closed.');
-      return;
-    }
+      if (roomRef.val().endedAt) {
+         alert('Room already closed.');
+         return;
+      }
 
-    history.push(`/rooms/${roomCode}`);
-  }
+      history.push(`/rooms/${roomCode}`);
+   }
 
-  return (
-    <div id="page-auth">
-      <aside>
-        <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
-        <strong>Crie salas de Q&amp;A ao-vivo</strong>
-        <p>Tire as dúvidas da sua audiência em tempo-real</p>
-      </aside>
-      <main>
-        <div className="main-content">
-          <img src={logoImg} alt="Letmeask" />
-          <button onClick={handleCreateRoom} className="create-room">
-            <img src={googleIconImg} alt="Logo do Google" />
-            Crie sua sala com o Google
-          </button>
-          <div className="separator">ou entre em uma sala</div>
-          <form onSubmit={handleJoinRoom}>
-            <input 
-              type="text"
-              placeholder="Digite o código da sala"
-              onChange={event => setRoomCode(event.target.value)}
-              value={roomCode}
-            />
-            <Button type="submit">
-              Entrar na sala
-            </Button>
-          </form>
-        </div>
-      </main>
-    </div>
-  )
+   return (
+      <div id="page-auth">
+         <aside>
+            <img src={illustrationImg} alt="Ilustração simbolizando perguntas e respostas" />
+            <strong>Crie salas de Q&amp;A ao-vivo</strong>
+            <p>Tire as dúvidas da sua audiência em tempo-real</p>
+         </aside>
+         <main>
+            <div className="main-content">
+               <img src={logoImg} alt="Letmeask" />
+               <button onClick={handleCreateRoom} className="create-room">
+                  <img src={googleIconImg} alt="Logo do Google" />
+                  Crie sua sala com o Google
+               </button>
+               <div className="separator">ou entre em uma sala</div>
+               <form onSubmit={handleJoinRoom}>
+                  <input
+                     type="text"
+                     placeholder="Digite o código da sala"
+                     onChange={event => setRoomCode(event.target.value)}
+                     value={roomCode}
+                  />
+                  <Button type="submit">
+                     Entrar na sala
+                  </Button>
+               </form>
+            </div>
+         </main>
+      </div>
+   )
 }
